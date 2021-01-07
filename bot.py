651f9df13b419d7fc256b9ad5b7789fc512e2813
isc-smalik <sachin.malik@intersystems.com>
2021-01-07 15:33:22 +0930

    help?

--- bot.py
+++ bot.py
@@ -11,11 +11,8 @@ class MyBot(ActivityHandler):
     async def on_message_activity(self, turn_context: TurnContext):
         await turn_context.send_activity(f"You said '{ turn_context.activity.text }'")
 
-    async def on_members_added_activity(
-        self,
-        members_added: ChannelAccount,
-        turn_context: TurnContext
-    ):
-        for member_added in members_added:
+    async def on_members_added_activity(self, members_added: ChannelAccount, turn_context: TurnContext):
+     for member_added in members_added:
             if member_added.id != turn_context.activity.recipient.id:
                 await turn_context.send_activity("Hello ! How may I help you ?")
+    
