1c6402110e35143befc52cb3409a7516770a9119
isc-smalik <sachin.malik@intersystems.com>
2021-01-07 16:19:53 +0930

    reaction

--- bot.py
+++ bot.py
@@ -2,10 +2,12 @@
 # Licensed under the MIT License.
 
 from botbuilder.core import ActivityHandler, TurnContext
-from botbuilder.schema import ChannelAccount
+from botbuilder.schema import ChannelAccount, MessageReaction
+from botbuilder.core.teams import TeamsActivityHandler
+from typing import List
 
 
-class MyBot(ActivityHandler):
+class MyBot(TeamsActivityHandler):
     # See https://aka.ms/about-bot-activity-message to learn more about the message and other activity types.
 
     async def on_message_activity(self, turn_context: TurnContext):
@@ -15,4 +17,11 @@ class MyBot(ActivityHandler):
      for member_added in members_added:
             if member_added.id != turn_context.activity.recipient.id:
                 await turn_context.send_activity("Hello ! How may I help you ?")
+
+    async def on_reactions_added(self, message_reaction : List[MessageReaction], turn_context : TurnContext):
+        for message_reaction in message_reaction:
+            await turn_context.send_activity(f"Your message reaction : '{message_reaction.type}'")
     
+    async def on_reactions_removed(self, message_reaction : List[MessageReaction], turn_context : TurnContext):
+        for message_reaction in message_reaction:
+            await turn_context.send_activity(f"You removed : '{message_reaction.type}'")
