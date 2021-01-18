555f099534ec0015cba56b5fa3abf2b8e300e0b5
isc-hacha <hacha@intersystems.com>
2021-01-19 09:05:07 +0930

    update

--- config.py
+++ config.py
@@ -1,6 +1,8 @@
 #!/usr/bin/env python3
 # Copyright (c) Microsoft Corporation. All rights reserved.
 # Licensed under the MIT License.
+# Microsoft App id : 4752eb87-8ffe-4677-981c-7159eb0688a2
+# Microsoft app pwd: __2gI8QcpWRq8G-svc9Xp4OGW-sY.uj3W~
 
 import os
 
@@ -8,5 +10,5 @@ class DefaultConfig:
     """ Bot Configuration """
 
     PORT = 3978
-    APP_ID = os.environ.get("MicrosoftAppId", "4752eb87-8ffe-4677-981c-7159eb0688a2")
-    APP_PASSWORD = os.environ.get("MicrosoftAppPassword", "__2gI8QcpWRq8G-svc9Xp4OGW-sY.uj3W~")
+    APP_ID = os.environ.get("MicrosoftAppId", "")
+    APP_PASSWORD = os.environ.get("MicrosoftAppPassword", "")
